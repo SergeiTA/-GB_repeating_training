2dc0022567336f6f7547a5b568043449ec5d88e7
SergeyTA <sergeyteral@gmail.com>
2020-12-08 11:01:14 +0400

    Cart table and Composite FOREIGN KEY

--- DBMySQL/repeat 1.sql
+++ DBMySQL/repeat 1.sql
@@ -81,19 +81,81 @@
 -- SELECT*FROM `category` LIMIT 2;
 -- SELECT*FROM `category` WHERE `discount` <> 0 LIMIT 2;
 
-USE repeat_shop; -- == Choose the database/scheme == ---
 
-SELECT*FROM category;
+
+-- SELECT*FROM category;
 -- INSERT INTO repeat_shop.category (`id`, `name`, `discount`) VALUES (5, "Шляпы", 0);
 
 -- == UPDATE updating a cell value with WHERE operator
 -- UPDATE category SET name = "Головные уборы" WHERE id = 5;
 -- UPDATE category SET discount = 3 WHERE id = 2 OR id = 5;
 
--- == DELETE data from row with WHERE operator
--- DELETE FROM category WHERE id = 5;
+-- == DELETE data from row with WHERE operator  
+-- DELETE FROM category WHERE id  = 5;
 -- UPDATE category SET alias_name = "mens clothes" WHERE id = 2;
 -- UPDATE category SET alias_name = "Men's shoes" WHERE id = 4;
+
+-- == Setting up the foreign key between `product` table `brand_id` column and `brand` table `id` column/ Name of the key is `fk_brand_product`
+-- ALTER TABLE `repeat_shop`.`product` ADD CONSTRAINT `fk_brand_product` FOREIGN KEY (`brand_id`) REFERENCES `repeat_shop`.`brand` (`id`) 
+-- ON DELETE NO ACTION -- actions then delete cell. If it would be CASCADE the deletion of the id from the `brand` table will result in a deletion rows with this brand is in the associated table `product`
+-- ON UPDATE NO ACTION; -- actions then update cell
+
+
+-- == DIAGRAAMMA / SHEME  click on "Database" - "Reverce Engineer" - chhode your srerver - choose ypor batabase / cheme
+
+-- ALTER TABLE `repeat_shop`.`product` ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `repeat_shop`.`category` (`id`)
+-- ON DELETE NO ACTION
+-- ON UPDATE NO ACTION;
+
+-- == Made `order` table == --
+-- CREATE TABLE `repeat_shop`.`order` (
+-- `id` INT NOT NULL AUTO_INCREMENT,
+-- `user_name` VARCHAR(128) NOT NULL,
+-- `phone` VARCHAR(32) NOT NULL,
+-- `datetime` DATETIME NOT NULL,
+-- PRIMARY KEY (`id`));
+
+-- == Filling out the `order` table with INSERT INTO command
+-- INSERT INTO `repeat_shop`.`order` (user_name, phone, datetime) VALUES ('Василий', '555-55-55', '2016-05-09 14^20');
+
+
+
+-- == Made `order_products` table for cart (otherwise, table "order" couldn't be a "normal" type of a table)
+-- CREATE TABLE `repeat_shop`.`order_products` (
+-- `order_id` INT NOT NULL,
+-- `product_id` INT NOT NULL,
+-- `count` INT NOT NULL,
+-- PRIMARY KEY (`order_id`, `product_id`));
+
+-- == Removing "NOT NULL" attribute from `count` column
+-- ALTER TABLE `repeat_shop`.`order_products` 
+-- CHANGE COLUMN `count` `count` INT NULL ;
+
+-- == Adding products into the order (cart) == --
+-- INSERT INTO order_products (order_id, product_id, count) VALUES (1, 1, 1);
+-- INSERT INTO order_products (order_id, product_id, count) VALUES (1, 2, 3);
+
+-- == Добавляем составной внешний ключ
+-- ALTER TABLE `repeat_shop`.`order_products` ADD INDEX `fk_order_products_idx` (`product_id` ASC);
+
+-- ALTER TABLE `repeat_shop`.`order_products` ADD CONSTRAINT `fk_order_products_order` FOREIGN KEY (`order_id`) REFERENCES `repeat_shop`.`order` (`id`) 
+-- ON DELETE NO ACTION 
+-- ON UPDATE NO ACTION; 
+
+-- ALTER TABLE `repeat_shop`.`order_products` ADD CONSTRAINT `fk_order_products_product` FOREIGN KEY (`product_id`) REFERENCES `repeat_shop`.`product` (`id`)
+-- ON DELETE NO ACTION
+-- ON UPDATE NO ACTION;
+-- == ---
+
+USE repeat_shop; -- == Choose the database/scheme == ---
+
+-- == Change "NULL" to "NOT NULL" attribute from `count` column
+-- ALTER TABLE  `repeat_shop`.`order_products` 
+-- CHANGE COLUMN `count` `count` INT NOT NULL;
+
+-- DESCRIBE `repeat_shop`.`order_products`;
+
+SELECT*FROM `order_products`;
 
 
 
