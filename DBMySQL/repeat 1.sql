243fbade749f2c6efa7eb21e330ba4cc14033c2c
SergeyTA <sergeyteral@gmail.com>
2021-02-10 12:43:54 +0400

    Outer tasks part 2

--- DBMySQL/repeat 1.sql
+++ DBMySQL/repeat 1.sql
@@ -474,14 +474,88 @@ USE `repeat_shop`;
 -- UPDATE `result` SET `result`.`result` = `result`.`result` + 2 WHERE `result`.`competition_id` IN (SELECT `competition`.`competition_id` AS `A` FROM `competition`
 	-- WHERE year(`competition`.`set_date`) < 2004);
 
-USE `sport`;
+-- USE `sport`;
 -- UPDATE `result` SET `result`.`result` = `result`.`result` - 2 WHERE `result`.`competition_id` = (SELECT `aaa` FROM 
 -- (SELECT `competition`.`competition_id` AS `aaa` FROM `competition`
 	-- INNER JOIN `result` ON `competition`.`competition_id` = `result`.`competition_id`
 	-- WHERE year(`competition`.`set_date`) < 2004 AND `result`.`result` = 50) AS `a` ); 
 
-DELETE FROM `sportsman` WHERE `sportsman`.`sportsman_id` = (SELECT `rec` FROM (
-	SELECT `sportsman`.`sportsman_id` AS `rec` FROM `sportsman` WHERE `sportsman`.`personal_record` = "AAA") AS `a`);
+-- DELETE FROM `sportsman` WHERE `sportsman`.`sportsman_id` = (SELECT `rec` FROM (
+	-- SELECT `sportsman`.`sportsman_id` AS `rec` FROM `sportsman` WHERE `sportsman`.`personal_record` = "AAA") AS `a`);
+
+-- CREATE TABLE `sport`.`result` (
+	-- `competition_id` INT NOT NULL,
+    -- `sportsman_id` INT NOT NULL,
+    -- `result` INT NOT NULL,
+    -- `city` VARCHAR(45),
+    -- `hold_date` DATE NOT NULL,
+-- PRIMARY KEY(`competition_id`, `sportsman_id`));
+
+-- CREATE SCHEMA `test`;
+
+-- CREATE TABLE `test`.`salespeople` (
+	-- `snum` INT NOT NULL AUTO_INCREMENT,
+    -- `sname` VARCHAR(45) NOT NULL,
+    -- `city` VARCHAR(45) NOT NULL,
+    -- `comm` DOUBLE NOT NULL,
+-- PRIMARY KEY (`snum`));   
+
+-- ALTER TABLE `salespeople` CHANGE COLUMN `cnum` `snum` INT NOT NULL AUTO_INCREMENT;
+
+-- CREATE TABLE `customers` (
+	-- `cnum` INT NOT NULL AUTO_INCREMENT,
+    -- `cname` VARCHAR(45) NOT NULL,
+    -- `city` VARCHAR(45) NOT NULL,
+    -- `rating` INT NOT NULL,
+    -- `snum` INT NOT NULL,
+-- PRIMARY KEY (`cnum`));
+
+-- CREATE TABLE `orders` (
+-- 	`onum` INT NOT NULL AUTO_INCREMENT,
+--     `amt` DOUBLE NOT NULL,
+--     `odate` DATE NOT NULL,
+--     `cnum` INT NOT NULL,
+--     `snum` INT NOT NULL,
+-- PRIMARY KEY (`onum`));
+
+USE `test`;
+-- INSERT INTO `test`.`salespeople` (`snum`, `sname`, `city`, `comm`) VALUES (1002, "Serres", "San Jose", 0.13);    
+-- INSERT INTO `salespeople` (`snum`, `sname`, `city`, `comm`) VALUES (1007, "Rifkin", "Barselona", 0.15);
+-- INSERT INTO `salespeople` (`snum`, `sname`, `city`, `comm`) VALUES (1001, "Peel", "London", 0.12);
+-- INSERT INTO `salespeople` (`snum`, `sname`, `city`, `comm`) VALUES (1004, "Motika", "London", 0.11);
+-- INSERT INTO `salespeople` (`snum`, `sname`, `city`, `comm`) VALUES (1003, "Axelrod", "New York", 0.1);
+
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2001, "Hoffman", "London", 100, 1001);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2002, "Giovanni", "Rome", 200, 1003);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2003, "Liu", "San Jose", 200, 1002);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2004, "Grass", "Berlin", 300, 1002);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2006, "Clemens", "London", 100, 1001);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2007, "Pereira", "Rome", 100, 1004);
+-- INSERT INTO `customers` (`cnum`, `cname`, `city`, `rating`, `snum`) VALUES (2008, "Cineros", "SanJose", 300, 1007);
+
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3001, 18.69, '2015-03-10', 2008, 1007);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3007, 75.75, '2015-04-10', 2004, 1002);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3003, 767.19, '2015-03-10', 2001, 1001);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3006, 1098.16, '2015-03-10', 2008, 1007);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3010, 1309.95, '2015-06-10', 2004, 1002);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3009, 1713.23, '2015-04-10', 2002, 1003);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3002, 1900.1, '2015-03-10', 2007, 1004);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3008, 4723, '2015-05-10', 2006, 1001);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3005, 5160.45, '2015-03-10', 2003, 1002);
+-- INSERT INTO `orders` (`onum`, `amt`, `odate`, `cnum`, `snum`) VALUES (3011, 9891.88, '2015-06-10', 2006, 1001);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
